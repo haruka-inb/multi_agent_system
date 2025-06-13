0b651efadc30ae3affed7d3bd31e6e3d5c42b01c
Haruka Inoba <haruka.inoba2011@gmail.com>
2025-06-13 17:03:59 +0200

    modifided path

--- src/ai_cv_advisor/app/api.py
+++ src/ai_cv_advisor/app/api.py
@@ -17,8 +17,9 @@ static_path = Path(__file__).parent / "static"
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 
-UPLOAD_DIR = Path("uploads")
-OUTPUT_DIR = Path("output")
+
+UPLOAD_DIR = Path.cwd() / "uploads"
+OUTPUT_DIR = Path.cwd() / "output"
 MARKDOWN_FILE = OUTPUT_DIR / "result.md"
 
 UPLOAD_DIR.mkdir(exist_ok=True)
